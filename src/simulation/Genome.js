9dffb4147018e1085cf088af15152be26e236dcd
Арсений Федотов <arsenya.fedotov.06@mail.ru>
2026-06-09 17:09:40 +0700

    Add Genome commit

---
+++ src/simulation/Genome.js
@@ -0,0 +1,10 @@
+class Genome {
+    constructor(options) {
+        this.speed = options.speed
+        this.size = options.size
+        this.visionRadius = options.visionRadius
+
+        this.mutationRate = options.mutationRate
+    }
+    mutate() {}
+}
